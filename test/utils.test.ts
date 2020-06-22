e4915b37062b6f43a1a4334f0069ec5045dde5ad
Johannes T <12032626+HannesT117@users.noreply.github.com>
2020-06-22 10:51:39 +0200

    fix(export): escape all special chars in module name (#14)

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -8,6 +8,7 @@ describe('a moduleName function', () => {
         {input: 'hello-World', expect: 'TypeHello__World'},
         {input: '0helloWorld', expect: 'Type0helloWorld'},
         {input: '0-helloWorld', expect: 'Type0__helloWorld'},
+        {input: '1-0-helloWorld', expect: 'Type1__0__helloWorld'},
         {input: 'hello World', expect: 'TypeHelloWorld'},
         {input: 'hello-World', expect: 'TypeHello__World'},
         {input: '12345', expect: 'Type12345'},
