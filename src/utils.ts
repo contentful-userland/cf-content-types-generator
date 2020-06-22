dfb95800294945e4c2d1c922fbabf491b983def3
Johannes Trepesch <12032626+HannesT117@users.noreply.github.com>
2020-06-22 10:47:28 +0200

    fix(export): escape all special chars in module name

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,8 @@ import {upperFirst} from 'lodash';
 import {pipe} from 'lodash/fp';
 
 export const moduleName = (name: string): string => {
-    const removeSpace = (input: string): string => input.replace(' ', '');
-    const replaceDash = (input: string): string => input.replace('-', '__');
+    const removeSpace = (input: string): string => input.replace(/\s/g, '');
+    const replaceDash = (input: string): string => input.replace(/-/g, '__');
     const addPrefix = (input: string): string => input.startsWith('Type') ? input : `Type${input}`;
     return pipe([replaceDash, upperFirst, addPrefix, removeSpace])(name);
 };
