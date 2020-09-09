fb4dcec3a5fd217148fc29f10173ade7217adca7
Hanna Greaves <hannagreaves@dropbox.com>
2020-09-09 16:34:09 +0100

    fix(validations): Add guards for potentially undefined field (#22)
    * Add guards for potentially undefined field
    
    * Add render test

--- src/renderer/cf-render-prop-link.ts
+++ src/renderer/cf-render-prop-link.ts
@@ -5,11 +5,11 @@ import {renderUnionType} from './render-union-type';
 
 const linkContentType = (field: Pick<Field, 'id' | 'validations'>): string => {
     const validations = linkContentTypeValidations(field);
-    return renderUnionType(validations.length > 0 ? validations.map(moduleFieldsName) : ['any']);
+    return renderUnionType(validations?.length > 0 ? validations.map(moduleFieldsName) : ['any']);
 };
 
 export const renderPropLink = (field: Pick<Field, 'id' | 'validations' | 'linkType'>) => {
-    const value = field.validations.length === 0 ? 'any' : linkContentType(field);
+    const value = field.validations && field.validations.length === 0 ? 'any' : linkContentType(field);
     return field.linkType === 'Entry'
         ? renderGenericType('Contentful.' + field.linkType, value)
         : 'Contentful.' + field.linkType!;
