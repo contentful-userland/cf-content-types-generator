34b5da7102abfe38c9c15cbaacc49bc8d4696c0a
Marco Link <marco.link@contentful.com>
2021-08-10 12:55:53 +0200

    fix linting error for base class

--- src/renderer/type/base-content-type-renderer.ts
+++ src/renderer/type/base-content-type-renderer.ts
@@ -4,9 +4,7 @@ import {ContentTypeRenderer} from './content-type-renderer';
 import {createDefaultContext, RenderContext} from './create-default-context';
 
 export class BaseContentTypeRenderer implements ContentTypeRenderer {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
-    setup(project: Project): void {
-    }
+    setup(project: Project): void {/**/}
 
     public render(contentType: CFContentType, file: SourceFile): void {
         file.addStatements(`/* Types for ${contentType.sys.id} */`);
