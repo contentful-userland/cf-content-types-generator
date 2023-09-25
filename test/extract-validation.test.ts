09bc9f305fc87791cd32c01a7e6dd4d11e979989
Marco Link <marco.link@contentful.com>
2023-09-25 15:26:37 +0200

    fix: remove patch-package [#284] (#286)

--- test/extract-validation.test.ts
+++ test/extract-validation.test.ts
@@ -23,6 +23,8 @@ describe('A linkContentTypeValidations function', () => {
 
   it('parses a non array linked content type validation', () => {
     const field = { validations: [{ linkContentType: 'topicA' }] };
+    // https://github.com/contentful-userland/cf-content-types-generator/issues/148
+    // @ts-ignore
     expect(linkContentTypeValidations(field)).toEqual(['topicA']);
   });
 });
