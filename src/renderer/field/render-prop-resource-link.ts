7304cdb6b3851a1550a5e2a3e95986418816dbaf
Alec Mitchell <alecpm@gmail.com>
2025-04-22 02:54:10 -0700

    feat: accept resource links to native external references in v10 type generator (#407)
    Trying to use `cf-content-types-generator` with a Contentful schema that
    includes fields with native external references currently results in an
    exception and failure to generate types. This PR updates
    `renderPropResourceLinkV10` to recognize the `ExternalResourceLink` type
    and allow it. When encountering an unknown `allowedResources` value it
    logs the value and treats it as `EntryFieldTypes.ExternalResourceLink`.
    Includes updated tests.

--- src/renderer/field/render-prop-resource-link.ts
+++ src/renderer/field/render-prop-resource-link.ts
@@ -18,21 +18,40 @@ export const renderPropResourceLink = (field: ContentTypeField, context: RenderC
   return renderTypeGeneric('Entry', 'Record<string, any>');
 };
 
+const EntryResourceType = renderTypeGeneric('EntryFieldTypes.EntryResourceLink', 'EntrySkeletonType');
+const ExternalResourceType = 'EntryFieldTypes.ExternalResourceLink';
+
 export const renderPropResourceLinkV10 = (
   field: ContentTypeField,
   context: RenderContext,
 ): string => {
+  const resourceTypes = new Set() as Set<string>;
   for (const resource of field.allowedResources!) {
     if (resource.type !== 'Contentful:Entry') {
-      throw new Error(`Unknown type "${resource.type}"`);
+      if (resource.source) {
+        // We have a cross-space reference that's not to an Entry, throw an error
+        throw new Error(`Unknown type "${resource.type}"`);
+      }
+
+      // Handle native external references
+      console.log(`Unknown external (?) resource link type "${resource.type}"`);
+      if (!resourceTypes.has(ExternalResourceType)) {
+        context.imports.add({
+            moduleSpecifier: 'contentful',
+            namedImports: ['EntryFieldTypes', 'ExternalResourceLink'],
+            isTypeOnly: true,
+        });
+        resourceTypes.add(ExternalResourceType);
+      }
+    } else if (!resourceTypes.has(EntryResourceType)) {
+      context.imports.add({
+        moduleSpecifier: 'contentful',
+        namedImports: ['EntryFieldTypes', 'EntrySkeletonType'],
+        isTypeOnly: true,
+      });
+      resourceTypes.add(EntryResourceType);
     }
   }
 
-  context.imports.add({
-    moduleSpecifier: 'contentful',
-    namedImports: ['EntryFieldTypes', 'EntrySkeletonType'],
-    isTypeOnly: true,
-  });
-
-  return renderTypeGeneric('EntryFieldTypes.EntryResourceLink', 'EntrySkeletonType');
+  return [...resourceTypes].join(" | ");
 };
--- test/renderer/field/render-prop-resource-link.test.ts
+++ test/renderer/field/render-prop-resource-link.test.ts
@@ -33,7 +33,7 @@ describe('A renderPropResourceLink function', () => {
     );
   });
 
-  it('rejects a "ResourceLink" with an unknown resource type', () => {
+  it('rejects a "ResourceLink" with a space and unknown resource type', () => {
     const field = JSON.parse(`
         {
           "id": "category",
@@ -115,6 +115,73 @@ describe('A renderPropResourceLinkV10 function', () => {
 
     expect(() => renderPropResourceLinkV10(field, createDefaultContext())).toThrow(
       'Unknown type "Contentful:UnknownEntity"',
+    );
+  });
+
+  it('Treats a "ResourceLink" with an unknown resource type and no space as an external resource', () => {
+    const field = JSON.parse(`
+        {
+          "id": "category",
+          "name": "Category",
+          "type": "ResourceLink",
+          "localized": false,
+          "required": true,
+          "validations": [],
+          "disabled": false,
+          "omitted": false,
+          "allowedResources": [
+            {
+              "type": "ExternalProvider:ExternalReference"
+            }
+          ]
+          }
+      `);
+
+    expect(renderPropResourceLinkV10(field, createDefaultContext())).toEqual(
+      'EntryFieldTypes.ExternalResourceLink',
+    );
+  });
+
+  it('Handles "ResourceLink" with mix of cross-space and external references', () => {
+    const field = JSON.parse(`
+        {
+          "id": "category",
+          "name": "Category",
+          "type": "ResourceLink",
+          "localized": false,
+          "required": true,
+          "validations": [],
+          "disabled": false,
+          "omitted": false,
+          "allowedResources": [
+            {
+              "type": "Contentful:Entry",
+              "source": "crn:contentful:::content:spaces/spaceId",
+                "contentTypes": [
+                "topicCategory",
+                "topicCategory2"
+              ]
+            },
+            {
+              "type": "Contentful:Entry",
+              "source": "crn:contentful:::content:spaces/spaceId2",
+                "contentTypes": [
+                "topicCategory3",
+                "topicCategory4"
+              ]
+            },
+            {
+              "type": "ExternalProvider:ExternalReference1"
+            },
+            {
+              "type": "ExternalProvider:ExternalReference2"
+            }
+          ]
+        }
+      `);
+
+    expect(renderPropResourceLinkV10(field, createDefaultContext())).toEqual(
+      'EntryFieldTypes.EntryResourceLink<EntrySkeletonType> | EntryFieldTypes.ExternalResourceLink',
     );
   });
 });
